f197af81226fdf31a110d01037aae010cabab23d
John Mars <JOHN@M4R5.io>
2015-08-23 11:22:01 -0400

    Add styling to page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-	res.send("John is " + (global.asleep ? "asleep" : "awake"));
+	res.send('<!DOCTYPE html><body style="margin:0;padding:0;"><div style="position:absolute;height:100%;width:100%;display:flex;align-items:center;justify-content:center;-webkit-box-align:center;-webkit-box-pack:center;color:white;background-color:' + (global.asleep ? '#2c3e50' : '#3498db') + ';"><div style="max-width:50%;flex:none;-webkit-box-flex:0;font-size:2em;font-family:sans-serif">' + 'John is ' + (global.asleep ? 'asleep' : 'awake') + '</div></div></body>');
 });
 
 app.get('/status', function (req, res) {
